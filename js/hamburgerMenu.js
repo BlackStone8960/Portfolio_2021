b45eaefb9f73a827dece13e1303bf4dcf8208b41
Taichi I <fng.trf.kk1020@gmail.com>
2021-02-13 00:28:49 -0800

    add slide show function

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -1,16 +1,15 @@
 'use strict';
 
+const hamburgerToggle = () => {
+  document.getElementById('hamburger-menu').classList.toggle('active');
+  document.getElementById('nav').classList.toggle('active');
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-  const hamburgerMenu = document.getElementById('hamburger-menu');
-  const nav = document.getElementById('nav');
+  document.getElementById('hamburger-menu').addEventListener('click', hamburgerToggle);
+  
   const navLinks = document.getElementsByClassName('nav-link');
-  hamburgerMenu.addEventListener('click', (e) => {
-    hamburgerMenu.classList.toggle('active');
-    nav.classList.toggle('active');
-  });
   for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener('click', (e) => {
-      nav.classList.toggle('active');
-    });
+    navLinks[i].addEventListener('click', hamburgerToggle);
   }
 })

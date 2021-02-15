00f66a2d5dbe9ae323f29d18e783654c07e1e444
Taichi I <fng.trf.kk1020@gmail.com>
2021-02-14 17:20:15 -0800

    Refactor swiperjs

--- js/swiper.js
+++ js/swiper.js
@@ -1,120 +1,69 @@
 'use strict';
 
+const cubeSlider = {
+  effect: 'cube',
+  loop: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+};
+
+const cubeSliderAuto = {
+  effect: 'cube',
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+};
+
+const cubeShadowOff = {
+  shadow: false,
+  slideShadows: false
+}
+
 if(!isSmartPhone()) {
-  let cubeSlider = new Swiper('#slider1', {
-    effect: 'cube',
-    loop: true,
-    // autoplay: {
-    //   delay: 10000,
-    //   disableOnInteraction: false,
-    // },
-    cubeEffect: {
-      shadow: true,
-      slideShadows: true,
-      shadowOffset: 20,
-      shadowScale: 0.94
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });  
+  let slider1 = new Swiper('#slider1', cubeSlider);  
 } else {
-  let cubeSlider = new Swiper('#slider1', {
-    effect: 'cube',
-    loop: true,
-    cubeEffect: {
-      shadow: false,
-      slideShadows: false
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });  
+  let slider1 = new Swiper('#slider1', {
+    ...cubeSlider,
+    cubeEffect: cubeShadowOff
+  });
   let slider2 = new Swiper('#slider2', {
-    effect: 'cube',
-    loop: true,
-    cubeEffect: {
-      shadow: false,
-      slideShadows: false
-    },
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    ...cubeSliderAuto,
+    cubeEffect: cubeShadowOff
   });
   let slider3 = new Swiper('#slider3', {
-    effect: 'cube',
-    loop: true,
-    cubeEffect: {
-      shadow: false,
-      slideShadows: false
-    },
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    ...cubeSliderAuto,
+    cubeEffect: cubeShadowOff
   });
   let slider4 = new Swiper('#slider4', {
-    effect: 'cube',
-    loop: true,
-    cubeEffect: {
-      shadow: false,
-      slideShadows: false
-    },
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    ...cubeSliderAuto,
+    cubeEffect: cubeShadowOff
   });
   let slider5 = new Swiper('#slider5', {
-    effect: 'cube',
-    loop: true,
-    cubeEffect: {
-      shadow: false,
-      slideShadows: false
-    },
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    ...cubeSliderAuto,
+    cubeEffect: cubeShadowOff
   });
 }
 
 const generateSwiper = (sliderId) => {
-  let cubeSlider = new Swiper(sliderId, {
-    effect: 'cube',
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    cubeEffect: {
-      shadow: true,
-      slideShadows: true,
-      shadowOffset: 20,
-      shadowScale: 0.94
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  let slider1 = new Swiper(sliderId, cubeSliderAuto);
 };

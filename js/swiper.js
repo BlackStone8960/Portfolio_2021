f8e69ba7e09bd7ce438416bb89b965000bccbce1
Taichi I <fng.trf.kk1020@gmail.com>
2021-02-15 18:27:14 -0800

    added some styles

--- js/swiper.js
+++ js/swiper.js
@@ -9,29 +9,22 @@ const cubeSlider = {
     shadowOffset: 20,
     shadowScale: 0.94
   },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
+  }
 };
 
 const cubeSliderAuto = {
-  effect: 'cube',
-  loop: true,
+  ...cubeSlider,
   autoplay: {
     delay: 4000,
     disableOnInteraction: false,
-  },
-  cubeEffect: {
-    shadow: true,
-    slideShadows: true,
-    shadowOffset: 20,
-    shadowScale: 0.94
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  }
 };
 
 const cubeShadowOff = {

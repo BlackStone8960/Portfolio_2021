fed0f08f90acce9c3d8d0c07fef344824fbab302
Taichi I <fng.trf.kk1020@gmail.com>
2021-02-16 23:56:00 -0800

    compressed a gif

--- js/switchImages.js
+++ js/switchImages.js
@@ -4,7 +4,6 @@ document.querySelectorAll('.thumb').forEach((item) => {
   let captureDiscription = document.getElementById(item.dataset.img);
   let descriptionClasses = captureDiscription.classList;
   let swiperId = "#" + captureDiscription.getElementsByClassName('swiper-container')[0].id;
-  let captureClass = item.getElementsByTagName('img')[0].classList;
 
   item.addEventListener('mouseover', () => {
     // Add hidden classes to every description parts
@@ -20,10 +19,6 @@ document.querySelectorAll('.thumb').forEach((item) => {
 
     // Remove active class from a capture
     document.getElementById('captures').getElementsByClassName('active')[0].classList.remove('active');
-    
-    // document.querySelectorAll('thumb').forEach((item) => {
-    //   captureClass.contains('active') === true && captureClass.remove('active');
-    // });
     item.getElementsByTagName('img')[0].classList.add('active');
   })
 });
